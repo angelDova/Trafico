6c1de77ea0871de077268439cb6bb1dbe5f87570
angelDova <Angelfcb01@yahoo.com>
2022-12-06 23:38:58 -0800

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { fadeInUp, footerTruckAnim, staggerFooterContainer } from "../variants";
+import { CgArrowLongRight } from "react-icons/cg";
+
+const Footer = ({ footerData }) => {
+  return <footer className="bg-pink-300">Footer</footer>;
+};
+
+export default Footer;
